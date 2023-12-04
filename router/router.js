3b250e60158e1192fd5982ae5ee9522d429fd77e
HeliosAiden <heliosaidenismyname@gmail.com>
2023-12-04 15:08:21 +0700

    c

--- router/router.js
+++ router/router.js
@@ -1,31 +1,37 @@
 import { urlRoutes, rootURL } from "../constants/routes.js";
 
-document.addEventListener("click", e => {
-    const {target} = e
-    if(!target.matches('#navigation a')) return ;
-    e.preventDefault();
-    urlRoute();
-})
+document.addEventListener("click", (e) => {
+  const { target } = e;
+  if (!target.matches("#navigation a")) return;
+  e.preventDefault();
+  urlRoute();
+});
 
 const urlRoute = (event) => {
-    event = event || window.event;
-    event.preventDefault();
-    window.history.pushState({},"",event.target.href);
-    urlLocationHandler();
-}
+  event = event || window.event;
+  event.preventDefault();
+  window.history.pushState({}, "", event.target.href);
+  urlLocationHandler();
+};
 
-const urlLocationHandler = async() => {
-    const location = window.location.pathname;
-    if (location.length == 0) {
-        location = "/"
-    }
+const urlLocationHandler = async () => {
+  const location = window.location.pathname;
+  if (location.length == 0) {
+    location = "/";
+  }
 
-    const route = urlRoutes[location] || urlRoutes[404];
-    const html = await fetch(`${rootURL}${route.pathName}`).then(res => res.text());
-    document.getElementById("content").innerHTML = html;
-}
+  const route = urlRoutes[location] || urlRoutes[404];
+  const html = await fetch(`${rootURL}${route.pathName}`).then((res) =>
+    res.text()
+  );
+  document.getElementById("content").innerHTML = html;
+  document.title = route.title;
+  document
+    .querySelector('meta[name="description"]')
+    .setAttribute("content", route.description);
+};
 
 window.onpopstate = urlLocationHandler;
 window.route = urlRoute;
 
-urlLocationHandler()
+urlLocationHandler();
